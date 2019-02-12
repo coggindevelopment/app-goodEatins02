e8fcbc1295e07fcfb6ebc819a5edfdf6ceb3f078
Nicholas Coggin <nicholascoggin@Nicholass-MacBook-Pro.local>
2019-02-12 05:54:16 -0600

    lesson-03 commit - Passing Data in PrepareForSegue

---
+++ GoodEatins/GoodEatins/Controller/RecipesSelectionVC.swift
@@ -0,0 +1,21 @@
+//
+//  RecipesSelectionVC.swift
+//  GoodEatins
+//
+//  Created by Nicholas Coggin on 2/12/19.
+//  Copyright © 2019 Nicholas Coggin. All rights reserved.
+//
+
+import UIKit
+
+class RecipesSelectionVC: UIViewController {
+    
+    var selectedCategory: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(selectedCategory)
+    }
+
+}
